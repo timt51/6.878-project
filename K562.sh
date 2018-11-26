be33da0ee97aa730629b146829304ca658b7d266
Timothy Fei Truong Jr <ttruong@mit.edu>
2018-11-26 16:30:37 -0500

    change dir before running

--- K562.sh
+++ K562.sh
@@ -1,17 +1,19 @@
+cd ./piq-single
+
 idx=0
 N=6
 for pwmid in {1..1316}
 do
     ((idx=idx%N)); ((idx++==0)) && wait
-    sudo Rscript ./piq-single/pwmmatch.exact.r ./piq-single/common.r ./piq-single/pwms/jasparfix.txt $pwmid ./data/K562/motif.matches &
+    sudo Rscript ./pwmmatch.exact.r ./common.r ./pwms/jasparfix.txt $pwmid ../data/K562/motif.matches &
 done
 
-sudo Rscript ./piq-single/bam2rdata.r ./piq-single/common.r ./data/K562/d0.Rdata ./data/K562/ENCFF441RET.bam
+sudo Rscript ./bam2rdata.r ./common.r ../data/K562/d0.Rdata ../data/K562/ENCFF441RET.bam
 
 task() {
-    sudo mkdir ./tmp$1/
-    sudo Rscript ./piq-single/pertf.r ./piq-single/common.r ./data/K562/motif.matches/ ./tmp$1/ ./data/K562/calls/ ./data/K562/d0.Rdata $1
-    sudo rm -rf ./tmp$1/
+    sudo mkdir ../tmp$1/
+    sudo Rscript ./pertf.r ./common.r ../data/K562/motif.matches/ ../tmp$1/ ../data/K562/calls/ ../data/K562/d0.Rdata $1
+    sudo rm -rf ../tmp$1/
 }
 
 idx=0
