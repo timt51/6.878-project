32951973f2e259df8094a7eec0c2e53d263b5326
Timothy Fei Truong Jr <ttruong@mit.edu>
2018-12-01 16:07:52 -0500

    grid search

--- train.py
+++ train.py
@@ -1,19 +1,23 @@
-import sys
+import argparse
 from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import f1_score, roc_auc_score
 
-mode = sys.argv[1]
-n_estimators = int(sys.argv[2])
-max_depth = int(sys.argv[3])
-cell_line = sys.argv[4]
-print("Training cell_line={} in mode {} with n_estimators={}, max_depth={}".format(cell_line, mode, n_estimators, max_depth))
+# Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode')
+parser.add_argument('--cell_line')
+args = parser.parse_args()
+mode = args.mode
+cell_line = args.cell_line
+print("Training cell_line={} in mode {}".format(cell_line, mode))
 
+# Get training data
 nonpredictors = ['enhancer_chrom', 'enhancer_start', 'enhancer_end', 'promoter_chrom', 'promoter_start', 'promoter_end', 'window_chrom', 'window_start', 'window_end', 'window_name', 'active_promoters_in_window', 'interactions_in_window', 'enhancer_distance_to_promoter', 'bin', 'label']
 
 orig_training_df = pd.read_hdf('./targetfinder/paper/targetfinder/'+cell_line+'/output-eep/training.h5', 'training').set_index(['enhancer_name', 'promoter_name'])
@@ -31,45 +35,60 @@ else:
     raise Exception("Unsupported mode")
 labels = training_df['label']
 
-estimator = GradientBoostingClassifier(n_estimators = n_estimators, learning_rate = 0.1, max_depth = max_depth, max_features = 'log2', random_state = 0)
-np.random.seed(0)
+# Get idxs per chrom
 idxs_per_chrom = {}
-for chrom in pd.unique(training_df['enhancer_chrom']):
+chroms = pd.unique(training_df['enhancer_chrom'])
+for chrom in chroms:
     training_df['enhancer_chrom'] == chrom
     idxs_per_chrom[chrom] = np.where(training_df['enhancer_chrom'] == chrom)[0]
 
-def train(args):
-    predictors_df, labels, estimator, idxs_per_chrom, chrom = args
-    train_idxs = np.array(list(set(range(len(predictors_df)))-set(idxs_per_chrom[chrom])),dtype=int)
+# Nested cross validation
+# parameters = {
+#     'n_estimators': [100, 200, 500, 1000, 4000],
+#     'max_depth': [2, 5, 10]
+# }
+parameters = {
+    'n_estimators': [100],
+    'max_depth': [2]
+}
+print("Proper Cross Validation")
+f1s, roc_aucs, importances = [], [], []
+for test_chrom in chroms:
     test_idxs = idxs_per_chrom[chrom].astype(int)
-    x_train, y_train = predictors_df.iloc[train_idxs], labels.iloc[train_idxs]
-    x_test, y_test = predictors_df.iloc[test_idxs], labels.iloc[test_idxs]
-    estimator = estimator.fit(x_train,y_train)
-    y_test_pred = estimator.predict(x_test)
-    y_test_probs = estimator.predict_proba(x_test)
-    return chrom, f1_score(y_test, y_test_pred), roc_auc_score(y_test, y_test_probs[:,1])
+    X_test, y_test = predictors_df.iloc[test_idxs], labels.iloc[test_idxs]
 
-print("Proper cross validation")
-k = 10
-pool = Pool(k)
-chroms = np.random.choice(list(idxs_per_chrom.keys()),k,replace=False)
-inputs_ = [(predictors_df, labels, estimator, idxs_per_chrom, chrom) for chrom in chroms]
-f1s, aucs = [], []
-for res in pool.imap_unordered(train, inputs_):
-    chrom, f1, auc = res
+    def cv(test_chrom):
+        for chrom in chroms-set(test_chrom):
+            train_idxs = np.array(list(set(range(len(predictors_df)))-set(idxs_per_chrom[chrom])-set(idxs_per_chrom[test_chrom])),dtype=int)
+            val_idxs = idxs_per_chrom[chrom].astype(int)
+            yield train_idxs, val_idxs
+
+    clf = GridSearchCV(GradientBoostingClassifier(learning_rate=0.1,max_features='log2',random_state=0), parameters, cv=cv(test_chrom), scoring='roc_auc')
+    clf.fit(predictors_df, labels)
+
+    y_test_pred = clf.predict(X_test)
+    y_test_probs = clf.predict_proba(X_test)
+    f1 = f1_score(y_test, y_test_pred)
+    roc_auc = roc_auc_score(y_test, y_test_probs[:,1])
     f1s.append(f1)
-    aucs.append(auc)
-    print(chrom, f1, auc)
-print("Avg perf")
-print(np.mean(f1s), np.mean(aucs))
+    roc_aucs.append(roc_auc)
+
+    this_importances = pd.Series(clf.best_estimator_.feature_importances_, index = predictors_df.columns).sort_values(ascending = False)
+    importances.append(this_importances)
+print("F1: {:.5f}, ROC_AUC: {:.5f}".format(np.mean(f1s), np.mean(roc_aucs)))
+for idx, chrom in enumerate(chroms):
+    print("{}\t{:.5f}\t{:.5f}".format(chrom, f1s[idx], roc_aucs[idx]))
+for idx, chrom in enumerate(chroms):
+    print(chrom)
+    print(importances[idx].head(16))
 
 print("Improper cross validation")
+estimator = GradientBoostingClassifier(n_estimators = 4000, learning_rate = 0.1, max_depth = 5, max_features = 'log2', random_state = 0)
 cv = StratifiedKFold(n_splits = 10, shuffle = True, random_state = 0)
 
 scores = cross_val_score(estimator, predictors_df, labels, scoring = 'roc_auc', cv = cv, n_jobs = -1)
 print('{:2f} {:2f}'.format(scores.mean(), scores.std()))
 
-# import pdb; pdb.set_trace()
-# estimator.fit(predictors_df, labels)
-# importances = pd.Series(estimator.feature_importances_, index = predictors_df.columns).sort_values(ascending = False)
-# print(importances.head(16))
+estimator.fit(predictors_df, labels)
+importances = pd.Series(estimator.feature_importances_, index = predictors_df.columns).sort_values(ascending = False)
+print(importances.head(16))
