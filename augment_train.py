a2463349e832530a2f994931a00df6f78afb2f81
Timothy Fei Truong Jr <ttruong@mit.edu>
2018-12-08 22:11:16 -0500

    augment window

--- augment_train.py
+++ augment_train.py
@@ -36,11 +36,13 @@ def process_calls_df(args):
 
     enhancer_calls_counts = np.zeros((len(training_df),))
     promoter_calls_counts = np.zeros((len(training_df),))
+    window_calls_counts = np.zeros((len(training_df),))
     for training_idx, row in enumerate(training_df.iterrows()):
         row = row[1]
-        enhancer_chrom, promoter_chrom = row['enhancer_chrom'], row['promoter_chrom']
+        enhancer_chrom, promoter_chrom, window_chrom = row['enhancer_chrom'], row['promoter_chrom'], row['window_chrom']
         enhancer_start, enhancer_end = row['enhancer_start'], row['enhancer_end']
         promoter_start, promoter_end = row['promoter_start'], row['promoter_end']
+        window_start, window_end = row['window_start'], row['window_end']
 
         chr_calls = calls_locs_df.get(enhancer_chrom, None)
         chr_purities = calls_purities_df.get(enhancer_chrom, None)
@@ -57,9 +59,17 @@ def process_calls_df(args):
             for idx in range(start_idx, len(chr_calls)):
                 if chr_calls[idx] < promoter_end:
                     promoter_calls_counts[training_idx] = max(promoter_calls_counts[training_idx], chr_purities[idx])
+
+        chr_calls = calls_locs_df.get(window_chrom, None)
+        chr_purities = calls_purities_df.get(window_chrom, None)
+        if chr_calls is not None:
+            start_idx = bisect.bisect(chr_calls, window_start)
+            for idx in range(start_idx, len(chr_calls)):
+                if chr_calls[idx] < window_end:
+                    window_calls_counts[training_idx] = max(window_calls_counts[training_idx], chr_purities[idx])
     name = fs[0].split('/')[-1].split('-')[1]
     tqdm.write('Completing ' + str(calls_idx) + ' ' + name)
-    return (calls_idx, name, enhancer_calls_counts, promoter_calls_counts)
+    return (calls_idx, name, enhancer_calls_counts, promoter_calls_counts, window_calls_counts)
 
 # Main
 cell_line = sys.argv[1]
@@ -74,12 +84,19 @@ for name in calls_files:
     else:
         inputs.append([name])
 input_args = [(inputs[i], training_df, i) for i in range(len(inputs))]
+df_no_window = training_df.copy()
+df_window = training_df.copy()
 for ret in tqdm(pool.imap(process_calls_df, input_args), total=len(input_args)):
-    calls_idx, name, enhancer_calls_counts, promoter_calls_counts = ret
+    calls_idx, name, enhancer_calls_counts, promoter_calls_counts, window_calls_counts = ret
     col_name = str(calls_idx) + ' ' + name
-    training_df[col_name + ' (Enhancer)'] = pd.Series(enhancer_calls_counts, index=training_df.index)
-    training_df[col_name + ' (Promoter)'] = pd.Series(promoter_calls_counts, index=training_df.index)
+    df_no_window[col_name + ' (Enhancer)'] = pd.Series(enhancer_calls_counts, index=df_no_window.index)
+    df_no_window[col_name + ' (Promoter)'] = pd.Series(promoter_calls_counts, index=df_no_window.index)
+
+    df_window[col_name + ' (Enhancer)'] = pd.Series(enhancer_calls_counts, index=df_window.index)
+    df_window[col_name + ' (Promoter)'] = pd.Series(promoter_calls_counts, index=df_window.index)
+    df_window[col_name + ' (Window)'] = pd.Series(window_calls_counts, index=df_window.index)
 
 print('Saving...')
-training_df.to_hdf('./targetfinder/paper/targetfinder/'+cell_line+'/output-eep/augmented_training.h5', key='training')
+df_no_window.to_hdf('./targetfinder/paper/targetfinder/'+cell_line+'/output-epw/augmented_no_window_training.h5', key='training')
+df_window.to_hdf('./targetfinder/paper/targetfinder/'+cell_line+'/output-epw/augmented_window_training.h5', key='training')
 print('Done')
